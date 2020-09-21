7518a4de9ca6c2853092957e83e49d9019748543
mamidiprasanna97 <mamidiprasanna97@gmail.com>
2020-09-21 14:21:20 -0400

    commit message

---
+++ js/button.js
@@ -0,0 +1,7 @@
+function myFunction()
+{
+    document.getElementById('myImage').src='img/app2.jpg';
+    document.getElementById("btn1").disabled = true;
+    document.getElementById("btn1").innerHTML ="button is clicked";
+    
+}
